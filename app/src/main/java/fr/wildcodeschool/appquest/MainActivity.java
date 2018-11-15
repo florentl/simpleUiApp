8208e4eb360d4ba80ed877a2368b7a1009882614
flo <flo33.lav@gmail.com>
2018-11-15 15:28:37 +0100

    gestion de la checkbox avant validation

--- app/src/main/java/fr/wildcodeschool/appquest/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/appquest/MainActivity.java
@@ -17,35 +17,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final EditText firstNameINput = findViewById(R.id.firstName);
+        final EditText lastNameInput = findViewById(R.id.lastName);
+        final CheckBox myCb = findViewById(R.id.checkBox);
 
         final Button button = findViewById(R.id.submit_btn);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Context context = getApplicationContext();
-                String firstName   = ((EditText)findViewById(R.id.firstName)).getText().toString();
-                String lastName   = ((EditText)findViewById(R.id.lastName)).getText().toString();
-                boolean inputFilled = firstName == null || "".equals(firstName.trim())
-                        || lastName == null || "".equals(lastName.trim());
+                String firstName   = firstNameINput.getText().toString();
+                String lastName   = lastNameInput.getText().toString();
+                boolean inputFilled = "".equals(firstName.trim()) || "".equals(lastName.trim());
                 if(inputFilled) {
                     String msg = context.getString(R.string.fillFirstLastName);
                     int duration = Toast.LENGTH_SHORT;
                     Toast toast = Toast.makeText(context, msg, duration);
                     toast.show();
                 } else {
-                    TextView congratTextView = (TextView)findViewById(R.id.welcomeMsg);
+                    TextView congratTextView = findViewById(R.id.welcomeMsg);
                     congratTextView.setText(context.getString(R.string.congrats));
                     congratTextView.append(" " + firstName + " " + lastName);
+                    firstNameINput.setText("");
+                    lastNameInput.setText("");
+                    myCb.setChecked(false);
+                    firstNameINput.setEnabled(false);
+                    lastNameInput.setEnabled(false);
                 }
             }
         });
 
 
-        final CheckBox myCb = findViewById(R.id.checkBox);
         myCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                   findViewById(R.id.firstName).setEnabled(isChecked);
                   findViewById(R.id.lastName).setEnabled(isChecked);
+                  if(!isChecked) {
+                      firstNameINput.setText("");
+                      lastNameInput.setText("");
+                  }
             }
         });
 
